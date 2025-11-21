df7cb4450b6b963a20cd8bc5df89a38330349f51
florestomas <floresmancillatomas@gmail.com>
2025-11-21 15:26:24 -0300

    Separacion de componentes

---
+++ zoneless-calculator-fr/src/app/calculator/components/calculator/calculator.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'calculator',
+  imports: [],
+  templateUrl: './calculator.html',
+})
+export class Calculator { }
--- zoneless-calculator-fr/src/app/calculator/views/calculator-view/calculator-view.ts
+++ zoneless-calculator-fr/src/app/calculator/views/calculator-view/calculator-view.ts
@@ -1,9 +1,12 @@
+import { Calculator } from '@/calculator/components/calculator/calculator';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'calculator-view',
   standalone: true,
-  imports: [],
+  imports: [
+    Calculator
+  ],
   templateUrl: './calculator-view.html',
 })
 export class CalculatorView { } // aca esta la clase que exporta
