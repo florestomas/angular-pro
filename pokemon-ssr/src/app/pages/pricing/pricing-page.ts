26f69816aabb6faa44ba6dce89ec2a232bc6b6fe
florestomas <floresmancillatomas@gmail.com>
2025-12-04 17:40:36 -0300

    Titulo y Metatags - SEO Friendly

--- pokemon-ssr/src/app/pages/pricing/pricing-page.ts
+++ pokemon-ssr/src/app/pages/pricing/pricing-page.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pricing-page',
   imports: [],
   templateUrl: './pricing-page.html',
 })
-export default class PricingPage {}
+export default class PricingPage implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('Contact Page');
+    this.meta.updateTag({ name: 'description', content: 'Este es mi Contact Page' });
+    this.meta.updateTag({ name: 'og:title', content: 'About Page' });
+    this.meta.updateTag({ name: 'keywords', content: 'Hola,Mundo,Fernando,Tomas,AKA,Mansi' });
+  }
+}
