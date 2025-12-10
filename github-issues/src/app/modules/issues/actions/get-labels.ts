ec58b40e72cb92706ed10521ae0f751350f8f89a
florestomas <floresmancillatomas@gmail.com>
2025-12-10 13:00:31 -0300

    GitHub Api - GetLabels

---
+++ github-issues/src/app/modules/issues/actions/get-labels.ts
@@ -0,0 +1,20 @@
+import { sleep } from '@helpers/*';
+import { GitHubLabel } from '../interfaces/github-label.interface';
+
+export const getLabels = async (): Promise<GitHubLabel[]> => {
+  await sleep(1500);
+
+  try {
+    const resp = await fetch(`https://api.github.com/repos/angular/angular/labels`);
+
+    if (!resp.ok) throw 'Cant load labels';
+
+    const labels: GitHubLabel[] = await resp.json();
+
+    console.log(labels);
+
+    return labels;
+  } catch (error) {
+    throw 'Cant load labes';
+  }
+};
