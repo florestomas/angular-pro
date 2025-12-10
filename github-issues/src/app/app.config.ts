b0d0bbc7a92f656ef34472a156988649bbc23327
florestomas <floresmancillatomas@gmail.com>
2025-12-10 12:34:41 -0300

    tanStack Query - Instalacion

--- github-issues/src/app/app.config.ts
+++ github-issues/src/app/app.config.ts
@@ -1,12 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
+import { withDevtools } from '@tanstack/angular-query-experimental/devtools';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTanStackQuery(new QueryClient(), withDevtools()),
+  ],
 };
